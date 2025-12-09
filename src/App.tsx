c8ccffeb2234f6cf7e88406d506ad799960c8137
Romi Setiawan <romi36825@gmail.com>
2025-12-09 10:34:38 +0700

    chore: fix tailwind setup and verify project structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-blue-600 text-white min-h-screen flex items-center justify-center font-bold text-3xl">
+      Test Tailwind: Berhasil!
+    </div>
+  )
+}
+export default App;
